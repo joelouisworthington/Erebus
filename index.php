0889ee9965b03c40f063eb0371ba24143968bbc2
Harry Wiseman <harry@strawberrysoup.co.uk>
2018-10-23 10:47:35 +0100

    Added some stuff for us to work from

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+global $wp_query;
+
+use Timber\Timber;
+
+require_once "controllers/IndexController.php";
+
+$page = IndexController::indexAction();
+
+Timber::render( $page['templates'], $page['context'] );
 
