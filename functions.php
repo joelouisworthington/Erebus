02d0169f2aafdab1c2ea4fb2b21e24613837763d
Harry Wiseman <harry@strawberrysoup.co.uk>
2019-01-25 11:45:48 +0000

    Removed example code from the functions file

--- functions.php
+++ functions.php
@@ -1,12 +1,5 @@
 <?php
 
-$fieldGroups = new \NanoSoup\Erebus\ACF\FieldGroups\Testimonials();
-$fieldGroups->init();
-
-// Todo: This is for test purposes - we would need a loader
-$testimonials = new \NanoSoup\Erebus\ACF\Blocks\Testimonials();
-$testimonials->init();
-
 add_filter('allowed_block_types', 'allowedBlocks');
 
 function allowedBlocks($allowed_block_types)
